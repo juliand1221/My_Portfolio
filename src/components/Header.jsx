1ad097ced8f501b518fa8701589d2c4899ee9f27
Julian David Tenorio <judt1221@hotmail.com>
2022-11-29 21:37:38 -0500

    Arreglo de archivos

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+// import "./styles/header.css"
+// import logo from "../media/icon.png"
+
+// const Header=() => {
+//     return(
+//         <header className="header">
+//             <div className="container logo-nav-container">
+//                 <div className="logo">
+//                     <img className="circular-square" src={logo}/>
+//                 </div>
+
+//             </div>
+//         </header>
+//     )
+
+// };
+
+// export default Header;
---
+++ src/pages/Principal.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const Index=() => {
+    return (
+        <div>
+            <Header />
+        </div>
+
+    )
+};
+
+export default Index;
